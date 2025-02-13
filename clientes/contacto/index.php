0137b834756f3c86f5ed92a0834b54fbdfcb8f85
Douglas <douglasgv0502@gmail.com>
2025-02-13 04:42:08 +0100

    ubicacion actualizada
    dasd

--- clientes/contacto/index.php
+++ clientes/contacto/index.php
@@ -46,8 +46,7 @@ include '../../app/config.php';
       </div>
       <div class="col-md-8">
         <h2 class="text-center mb-4">Nuestra Ubicación</h2>
-        <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15682.335363660924!2d-71.68022006750103!3d10.689374965538416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e89995f0536d7c1%3A0x23dd7e8e4c437f3!2sCEDIVETCA!5e0!3m2!1ses!2sve!4v1721080814859!5m2!1ses!2sve" width="600" height="450" style="border: 0; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div>
+        <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3920.661140007966!2d-71.61375042495939!3d10.683388689459774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTDCsDQxJzAwLjIiTiA3McKwMzYnNDAuMiJX!5e0!3m2!1ses!2sve!4v1739418029701!5m2!1ses!2sve" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>      </div>
     </div>
   </div>
 </section>
